dc7a64a50b80e54abcac5b7083257995e7e97938
korneevaed <korneevaed@gmail.com>
2015-05-17 23:07:50 +0200

    made markers work by moving new position declaration away from marker consstructor; started to play with arrays to show only markers that are filtered

--- js/script.js
+++ js/script.js
@@ -16,6 +16,19 @@ var initialPlaces = [
 	}
 ];
 
+function Place(placeObj) {
+	this.name = placeObj.name;
+	this.position = new google.maps.LatLng(placeObj.lat, placeObj.longt);
+
+	var marker = new google.maps.Marker({
+		position: this.position,
+		title: name,
+		map: map,
+		draggable: true,
+		animation: google.maps.Animation.DROP
+	});
+}
+
 var mapProp = {
 	center: new google.maps.LatLng(52.531283, 13.422102),
 	zoom: 15,
@@ -26,36 +39,32 @@ var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
 var ViewModel = function () {
 	var self = this;
-	self.places = ko.observableArray([]);
-	initialPlaces.forEach( function(placeObj) {
-		self.places.push(new Place(placeObj));
-	});
 
 	self.userInput = ko.observable("");
+	self.places = ko.observableArray([]);
+	var filteredPlaces = [];
+	var testPlaces = [];
+
+	// need to show only filtered places
+
+	initialPlaces.forEach( function(placeObj) {
+		if (placeObj.name.indexOf(self.userInput()) >= 0) {
+			filteredPlaces.push(new Place(placeObj));
+		}
+	});
+
+	// filteredPlaces.forEach( function(placeObj) {
+	// 	self.places.push(new Place(placeObj));
+	// });
 
 	self.searchFilter = ko.pureComputed(function() {
-		var testVar = [];
 		initialPlaces.forEach( function(placeObj, i) {
 			if (placeObj.name.indexOf(self.userInput()) >= 0) {
-				testVar.push(placeObj.name);
-				console.log(testVar[i]);
+				testPlaces.push(placeObj.name);
 			}
 		});
-		return testVar;
+		return testPlaces;
 	}, this);
 };
-
-function Place(placeObj) {
-	this.name = placeObj.name;
-	this.lat = ko.observable(placeObj.lat);
-	this.longt = ko.observable(placeObj.longt);
-
-	var marker = new google.maps.Marker({
-		position: new google.maps.LatLng(this.lat, this.longt),
-		title: name,
-		map: map,
-		draggable: true
-	});
-}
 
 ko.applyBindings(new ViewModel());
