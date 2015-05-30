c700a93c5386023315c585ecc5c3b1e890256cf2
korneevaed <korneevaed@gmail.com>
2015-05-30 21:03:49 +0200

    Removed old places model. Organisationsl changes

--- js/script.js
+++ js/script.js
@@ -1,78 +1,3 @@
-var places = [
-	{
-		lat: 52.529648,
-		longt: 13.434989,
-		name: 'Cafe 1',
-		visible: true,
-		placeType: 'cafe',
-		infoContent: 'content replacement'
-	},
-	{
-		lat: 52.530875,
-		longt: 13.403017,
-		name: 'Cafe 2',
-		visible: true,
-		placeType: 'cafe',
-		infoContent: 'content replacement'
-	},
-	{
-		lat: 52.535259,
-		longt: 13.437249,
-		name: 'Cafe 3',
-		visible: true,
-		placeType: 'cafe',
-		infoContent: 'content replacement'
-	},
-	{
-		lat: 52.537371,
-		longt: 13.420482,
-		name: 'Shop 1',
-		visible: true,
-		placeType: 'shop',
-		infoContent: 'content replacement'
-	},
-	{
-		lat: 52.526216,
-		longt: 13.418327,
-		name: 'Shop 2',
-		visible: true,
-		placeType: 'shop',
-		infoContent: 'content replacement'
-	},
-	{
-		lat: 52.533257,
-		longt: 13.415247,
-		name: 'Shop 3',
-		visible: true,
-		placeType: 'shop',
-		infoContent: 'content replacement'
-	},
-	{
-		lat: 52.535369,
-		longt: 13.425480,
-		name: 'Bar',
-		visible: true,
-		placeType: 'misc',
-		infoContent: 'content replacement'
-	},
-	{
-		lat: 52.534214,
-		longt: 13.422325,
-		name: 'Library',
-		visible: true,
-		placeType: 'misc',
-		infoContent: 'content replacement'
-	},
-	{
-		lat: 52.533255,
-		longt: 13.437255,
-		name: 'Chirch',
-		visible: true,
-		placeType: 'misc',
-		infoContent: 'content replacement'
-	}
-];
-
 function initialize() {
 
 	var mapProp = {
@@ -83,12 +8,6 @@ function initialize() {
 
 	var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
-
-	google.maps.event.addListener(map, 'click', function() {
-		// self.markers()[1].infoWindow.open(map,self.markers()[1]);
-		console.log('la');
-	});
-
 	var ViewModel = function () {
 
 		var self = this;
@@ -98,17 +17,32 @@ function initialize() {
 		self.infoWindows = ko.observableArray([]);
 		self.isVisible = ko.observable(true);
 
+		var client_id = "4CPJVSPROXAN332ZFSRUGBVMW4LFWYOYMVTDEFQ2NOFUU42O";
+		var client_secret = "KSXNQ4KXF4SLJIQQ0UWMJR4ZWIXWGQ4CL4VW1D2IR1BC0XKV";
+		var location = "52.531283, 13.422102";
+		var dateAndTime = "20150515";
+		var url ="https://api.foursquare.com/v2/venues/search?ll="+location+
+			"&client_id="+client_id+
+			"&client_secret="+client_secret+
+			"&v="+dateAndTime;
+
+		$.getJSON( url, function(result, status) {
+			if (status !== 'success') return alert('Request to Foursquare failed, haha');
+			// Transform each venue result into a marker on the map.
+			self.applyMarkers(result.response.venues);
+		});
+
 		self.applyMarkers = function (foursqPlaces) {
 			for (var i = 0; i < foursqPlaces.length; i++) {
 				var foursqPlace = foursqPlaces[i];
 				var marker = new google.maps.Marker ({
-					position: new google.maps.LatLng(foursqPlaces[i].location.lat, foursqPlaces[i].location.lng),
+					position: new google.maps.LatLng(foursqPlace.location.lat, foursqPlace.location.lng),
 					map: map,
-					title: foursqPlaces[i].name,
+					title: foursqPlace.name,
 					visible: true,
 					koVisible: ko.observable(true),
-					placeType: foursqPlaces[i].categories.name,
-					infoWindow: new google.maps.InfoWindow({content: foursqPlaces[i].name})
+					placeType: foursqPlace.categories.name,
+					infoWindow: new google.maps.InfoWindow({content: foursqPlace.name})
 				});
 				self.markers.push(marker);
 
@@ -120,22 +54,6 @@ function initialize() {
 				})(marker));
 			}
 		}
-
-		var client_id = "4CPJVSPROXAN332ZFSRUGBVMW4LFWYOYMVTDEFQ2NOFUU42O";
-		var client_secret = "KSXNQ4KXF4SLJIQQ0UWMJR4ZWIXWGQ4CL4VW1D2IR1BC0XKV";
-		var location = "52.531283, 13.422102";
-		var dateAndTime = "20150515";
-		var url ="https://api.foursquare.com/v2/venues/search?ll="+location+
-			"&client_id="+client_id+
-			"&client_secret="+client_secret+
-			"&v="+dateAndTime+
-			"&query=sushi";
-
-		$.getJSON( url, function(result, status) {
-			if (status !== 'success') return alert('Request to Foursquare failed, haha');
-			// Transform each venue result into a marker on the map.
-			self.applyMarkers(result.response.venues);
-		});
 
 		self.filterMarkers = function () {
 			for (var i = 0; i < self.markers().length; i++) {
